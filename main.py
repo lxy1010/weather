bda16c6af1724d6997b17edd5a8be857f17bf1cd
lxy1010 <3329680030@qq.com>
2023-01-04 16:57:05 +0800

    完成 提交数据到 json

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+import requests
+import bs4
+import json
 
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+res = requests.get('http://t.weather.sojson.com/api/weather/city/101200901')
+res.raise_for_status()
+text = eval(bs4.BeautifulSoup(res.text, 'html.parser').text)
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+with open('package.json', 'w', encoding='utf-8') as f:
+    json.dump(text, f, indent=4)
+
+print(text['data']['shidu'])
